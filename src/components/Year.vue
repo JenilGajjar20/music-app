f618da8022e39b4302146983bcc67b4427de0d6f
Jenil Gajjar <68738624+JenilGajjar20@users.noreply.github.com>
2022-12-10 21:00:35 +0530

    [CHANGE] UI UPDATED

---
+++ src/components/Year.vue
@@ -0,0 +1,14 @@
+<template>
+  <div class="bg-primary-500 text-base py-1 px-2 w-fit rounded-lg">
+    <p>{{ songYear }}</p>
+  </div>
+</template>
+
+<script>
+export default {
+  name: "BadgeComp",
+  props: {
+    songYear: Number,
+  },
+};
+</script>
