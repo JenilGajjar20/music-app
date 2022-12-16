b6e4af1c20eb517baca9e57f9221319542cbc023
Jenil Gajjar <68738624+JenilGajjar20@users.noreply.github.com>
2022-12-16 08:51:21 +0530

    WIP

--- src/main.js
+++ src/main.js
@@ -3,6 +3,7 @@ import App from "./App.vue";
 import "./assets/css/tailwind.css";
 import "./assets/css/style.scss";
 import "./global";
+import "./plugins/index";
 
 Vue.config.productionTip = false;
 
